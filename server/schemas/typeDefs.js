f3449b12ae0bad055b3ddd130fcd2757275279bf
Jason Arritt <jsn.arritt@gmail.com>
2022-07-15 16:06:52 -0400

    Added me Query and initial mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -24,6 +24,24 @@ const typeDefs = gql`
     token: ID!
     user: User
   }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(
+      bookId: ID!
+      authors: [String]
+      description: String
+      title: String
+      image: String
+      link: String
+    ): User
+    removeBook(bookId: ID!): User
+  }
 `;
 
 // export the typeDefs
